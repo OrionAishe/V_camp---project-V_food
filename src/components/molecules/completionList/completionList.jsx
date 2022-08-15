ba1e620d2eecb800d5d25d32bfb70e632589ea44
Orion Antonucci <wagantonucci@hotmail.com>
2022-08-15 12:51:52 -0300

    finish Directions component

--- src/components/molecules/completionList/completionList.jsx
+++ src/components/molecules/completionList/completionList.jsx
@@ -4,14 +4,27 @@ import styles from "./completionList.module.scss";
 const CompletionList = ({title, list}) => {
 
     const onClickHandler = (e) => {
+        if(e.target.nodeName != "LI"){
+            e.target.parentElement.classList.toggle(styles["--active"]);
+        }
         e.target.classList.toggle(styles["--active"]);
     };
 
     return <div className={styles.CompletionList}>
         <Paragraph className={styles.List__Title}>{title}</Paragraph>
         <ul className={styles.List}>
-            {list.map((item) => {
-               return <li className={styles.List__Item} key={item.index} onClick={onClickHandler}><span className={styles.List__Checkbox}></span>{item}</li>
+            {title == "Ingredients" && list.map((item, index) => {
+               return <li className={styles.List__Item} key={index} onClick={onClickHandler}>
+                <span className={styles.List__Checkbox}></span>{item}</li>
+            })}
+            {title == "Directions" && list.map((item, index) => {
+                return <li className={styles.List__Direction} key={index} onClick={onClickHandler}>
+                    <Paragraph className={styles.List__DirectionTitle} >
+                        <span className={styles.List__Checkbox} />
+                        {index+1}. 
+                        {item.title}
+                    </Paragraph>
+                <Paragraph className={styles.List__Description} >{item.description}</Paragraph></li>
             })}
         </ul>
     </div>
--- src/components/organisms/ingredientsTable/ingredientsTable.jsx
+++ src/components/organisms/ingredientsTable/ingredientsTable.jsx
@@ -2,15 +2,27 @@ import { CompletionList, CardStack, RecipeCard, SiteBanner } from "@molecules";
 import styles from "./ingredientsTable.module.scss";
 
 const IngredientsTable = ({ recipes }) => {
-    const list = ["Lorem ipsum dolor sit amet.",
+    const ingredients = ["Lorem ipsum dolor sit amet.",
         "Lorem ipsum dolor sit amet.",
         "Lorem ipsum dolor sit amet.",
         "Lorem ipsum dolor sit amet.",
         "Lorem ipsum dolor sit amet."];
+
+    const directions = [{
+        title: "Lorem ipsum dolor sit amet.",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam minima similique distinctio excepturi tempora sit deserunt ab consequuntur quasi voluptas."
+    }, {
+        title: "Lorem ipsum dolor sit amet.",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam minima similique distinctio excepturi tempora sit deserunt ab consequuntur quasi voluptas."
+    }, {
+        title: "Lorem ipsum dolor sit amet.",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam minima similique distinctio excepturi tempora sit deserunt ab consequuntur quasi voluptas."
+    },];
+
     return <>
         <div className={styles.Ingredients}>
             <div className={styles.Ingredients__List}>
-                <CompletionList title="Ingredients" list={list} />
+                <CompletionList title="Ingredients" list={ingredients} />
             </div>
             <div className={styles["Ingredients__recipesStack"]}>
                 <h2 className={styles["Ingredients__recipesStackTitle"]}>
@@ -25,7 +37,7 @@ const IngredientsTable = ({ recipes }) => {
                 <SiteBanner />
             </div>
             <div className={styles.Ingredients__Directions}>
-                <CompletionList title="Directions" list={[]}/>
+                <CompletionList title="Directions" list={directions} />
             </div>
         </div>
     </>
